0a31f3a77d8e73820a10acc2f99372e8f959cb62
Shihab585203 <freelancersehab1@gmail.com>
2024-03-30 17:33:33 +0600

    added react helmet async instead of react helmet to solve an error

--- src/Components/PageTitle.jsx
+++ src/Components/PageTitle.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const PageTitle = ({ title }) => {
   return (
-    <Helmet>
-      <title>{title} || FoodCaB</title>
-    </Helmet>
+    <div>
+      <HelmetProvider>
+        <div>
+          <Helmet>
+            <title>{`${title} || FoodCaB`}</title>
+          </Helmet>
+        </div>
+      </HelmetProvider>
+    </div>
   );
 };
 
